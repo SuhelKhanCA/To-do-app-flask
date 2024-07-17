b4f1218b9ca32aa008e3779bc698c3f60658dc28
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-07-17 05:31:34 +0000

    tring for db test

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask_sqlalchemy import SQLAlcehmy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-    return 'Hello, World!'
 
 @app.route('/products')
 def products():
     return 'this is product page'
 
 if __name__== "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=5000)
